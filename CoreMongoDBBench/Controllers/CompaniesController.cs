4214fa0aab1132f79278f009c4e16bc378d1c135
Rodrigo <rodrigoarancibiapla@gmail.com>
2019-12-17 15:38:28 -0300

    second

---
+++ CoreMongoDBBench/Controllers/CompaniesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMongoDBBench.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+      
+        // GET: api/companies/20
+        [HttpGet("{number}", Name = "Get")]
+        public string Get(int number)
+        {
+            Models.MongoDataService model= new Models.MongoDataService(Environment.GetEnvironmentVariable("MONGOURI"), 
+                Environment.GetEnvironmentVariable("MONGODB"), 
+                Environment.GetEnvironmentVariable("MONGOCOLLECTION"));
+            Console.WriteLine("---");
+            return model.find(number);
+        
+        }
+
+      
+    }
+}
---
+++ CoreMongoDBBench/Models/MongoDataService.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMongoDBBench.Models
+{
+    public class MongoDataService
+    {
+        private IMongoCollection<BsonDocument> collection;
+        
+        public MongoDataService(string connectionString, string databaseName, string collectionName)
+        {
+            MongoClient client = new MongoClient(connectionString);
+            var database = client.GetDatabase(databaseName);
+            this.collection = database.GetCollection<BsonDocument>(collectionName);
+            
+        }
+
+        public string find(int number)
+        {
+            var filter = Builders<BsonDocument>.Filter.Gt("numEmps", number);
+            var documents = collection.Find(filter).ToList<BsonDocument>();
+
+            if (documents != null)
+            {
+                return documents.Take(100).ToJson();
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
